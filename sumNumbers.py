68309ccb0fb0797d0c0f8655158ab5aaa4f5e8a5
SANDHILLS\LH218-11 <jad.talbert@gmail.com>
2025-08-27 09:47:48 -0400

    adding a new file for testing

---
+++ sumNumbers.py
@@ -0,0 +1,13 @@
+#this program sums two numbers
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+sum = float(num1) + float(num2)
+
+print("the sum of {0} and {1} is {2}".format(num1,num2,sum))
+
+num3 = input("enter your 3rd number: ")
+num4 = input("enter your 4th number: ")
+diff = int(num4) - int(num3)
+
+print(diff)
