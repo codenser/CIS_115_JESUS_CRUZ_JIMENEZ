8acfafcdb670163b4aa7f2424924a7bab73ea32a
SANDHILLS\LH218-11 <jad.talbert@gmail.com>
2025-09-03 09:52:45 -0400

    new programs

---
+++ multiplyTwoNumbers.py
@@ -0,0 +1,8 @@
+
+# this program displays the product of two numbers
+num1 = input("enter first number: ")
+num2 = input("enter second number: ")
+
+product= int(num1) * int(num2)
+
+print(f' the product of {num1} and {num2} is {product}')
---
+++ squareRoot.py
@@ -0,0 +1,6 @@
+num=4
+
+# the double ** raises the value of the num variable to 1/2 or .5
+square_root = num ** .5
+
+print(square_root)
