1ce840586cb2d238165569924f48bf185d41b725
codenser <cruzjimenezj646@mail.sandhills.edu>
2025-09-04 09:00:33 -0400

    more programs!

---
+++ divideByZero.py
@@ -0,0 +1,8 @@
+# this program divides a number by zero
+
+num1 =input("Enter number: ")
+num2 = 0
+
+divide = float(num1) / float(num2)
+
+print("the divided point of {0} and {1} is {2}".format(num1,num2,divide))
