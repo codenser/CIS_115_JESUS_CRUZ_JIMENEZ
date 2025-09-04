107d654fd02a658cc584092d48e0d5423e1dab86
codenser <cruzjimenezj646@mail.sandhills.edu>
2025-09-04 09:40:24 -0400

    fav movie

---
+++ favoriteMovie.py
@@ -0,0 +1,9 @@
+#this program will display my favorite movie and display the main characters
+
+movie= "Wall-E"
+
+m1= "Wall-E"
+m2= "Eve"
+m3= "M-O"
+
+print(f'My favorite movie is {movie} and the main characters are {m1}, {m2}, and {m3}.')
