37e458beadaac09a0c2fc912077f9cee023e8088
codenser <cruzjimenezj646@mail.sandhills.edu>
2025-09-04 09:58:19 -0400

    updated favoriteMovie.py

--- favoriteMovie.py
+++ favoriteMovie.py
@@ -5,5 +5,11 @@ movie= "Wall-E"
 m1= "Wall-E"
 m2= "Eve"
 m3= "M-O"
+m4= "Captain B. Mcrea"
 
-print(f'My favorite movie is {movie} and the main characters are {m1}, {m2}, and {m3}.')
+print(f'My favorite movie is {movie} and the main characters are:')
+print(m1)
+print(m2)
+print(m3)
+print(m4)
+print("This movie was a childhood favorite of mine and I always enjoyed watching it growing up.")
