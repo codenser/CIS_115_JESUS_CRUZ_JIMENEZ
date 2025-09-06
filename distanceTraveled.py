ccce5ac735de4d77e133cf2c2768c2056a45f461
codenser <cruzjimenezj646@mail.sandhills.edu>
2025-09-06 01:03:41 -0400

    new programs

---
+++ distanceTraveled.py
@@ -0,0 +1,7 @@
+#this code uses a formula to calculate the distance traveled
+#euqation: distance = velocity
+velocity = 80
+time = 2
+distance = velocity * time
+
+print(f'the calculated distance traveled is {distance} miles.')
