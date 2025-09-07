3dc6690eadb2c577ae51a761f7067cf96a07caea
codenser <cruzjimenezj646@mail.sandhills.edu>
2025-09-06 20:21:20 -0400

    finished w1/w2 programs

---
+++ findForce.py
@@ -0,0 +1,9 @@
+#this program uses two numbers to calculate the force of an object
+#equation: Force = mass * acceleration
+
+mass = float(10)
+acceleration= float(9.81)
+
+force = mass * acceleration
+
+print(f'The calculated force of {mass} kg and {acceleration} m/s^2 is {force} Newtons.')
