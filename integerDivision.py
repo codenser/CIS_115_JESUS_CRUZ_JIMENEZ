db943faa9d0aebe28fba3ac884e6795258fc2ae7
codenser <cruzjimenezj646@mail.sandhills.edu>
2025-09-10 09:28:54 -0400

    Organization

---
+++ integerDivision.py
@@ -0,0 +1,5 @@
+#this program uses integer division to divide two floating point numbers
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+result = float(num1) // float(num2)
