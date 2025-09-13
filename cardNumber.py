0800c71718b3df0feead9b8dabc0a539d62d9a1b
codenser <cruzjimenezj646@mail.sandhills.edu>
2025-09-13 19:17:30 -0400

    Finished prgrams

---
+++ cardNumber.py
@@ -0,0 +1,6 @@
+#this program uses the input funtion to request card information
+cardNum = input("Enter card number: ")
+expD = input("Enter expiration date (MM/YY): ")
+sCode = input("Enter security code: ")
+
+print(f'The card number is {cardNum}, the expiration date is {expD}, and the security code is {sCode}')
---
+++ fakeAddress.py
@@ -0,0 +1,2 @@
+#this program prints a fake adress line by line
+print ("1234\nSpring St.\nSpringfield, OH 56789")
