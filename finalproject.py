f3e0f527476dfd918a186f47f7348414180c06bc
codenser <cruzjimenezj646@mail.sandhills.edu>
2025-11-09 15:30:33 -0500

    final project progress

---
+++ finalproject.py
@@ -0,0 +1,16 @@
+#this is my final project for CIS 115
+
+#create a catalog of products with prices
+def create_catalog():
+    catalog = {
+	"---------------------------------------------------------------"
+                          "PRODUCT CATALOG"
+	"---------------------------------------------------------------"
+	"1      |    USB Drive(128 GB)                      |   $12.00"
+	"2      |   Mac Book Pro(15 inch)                   |   $2900.00"
+	"3      |   Arduino 1010(with blue tooth)           |   $48.00"
+	"4      |   Ring Camera(wireless)                   |   $156.00"
+	"5      |   Smart TV(TCL 70 inch)                   |   $359.00"
+	"--------------------------------------------------------------"
+
+    }
